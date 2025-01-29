03f3efb94a25d0a5962a0ffcb02943a35a5bdc7b
Nuytten Baptiste <baptistenuytten@gmail.com>
2025-01-29 03:14:34 +0100

    Ajout du menu de navigation dans le jeu

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ typedef struct {
 int main() {
     char Username[MAX_USERNAME];
     int starterChoice;
+    int Location = 0;  // Initialiser Location à 0
     Pokemon starter;
 
     // Définir les Pokémon starters
@@ -27,7 +28,7 @@ int main() {
 
     printf("Bienvenue %s dans le monde Pokémon!\n", Username);
 
-    // Menu
+    // Menu pour choisir le starter
     printf("+-----------------------------------+\n");
     printf("|                                   |\n");
     printf("|       Choisir votre starter       |\n");
@@ -44,22 +45,56 @@ int main() {
     switch (starterChoice) {
         case 1:
             starter = charmander;
-        break;
+            break;
         case 2:
             starter = squirtle;
-        break;
+            break;
         case 3:
             starter = bulbasaur;
-        break;
+            break;
         default:
             printf("Choix invalide. Aucun Pokémon sélectionné.\n");
-        return 0;
+            return 0;
     }
 
     // Afficher les informations du starter choisi
     printf("Vous avez choisi %s !\n", starter.name);
     printf("Stats : HP = %d, Attaque = %d, Défense = %d\n", starter.hp, starter.attack, starter.defense);
     printf("Bonne chance pour votre aventure, %s !\n", Username);
+
+    // Menu des options d'action
+    while (1) {
+        printf("+-------------------------------------+\n");
+        printf("|                                     |\n");
+        printf("|       Ou voulez-vous aller ?        |\n");
+        printf("|                                     |\n");
+        printf("|       1 - La nature                 |\n");
+        printf("|       2 - Shop                      |\n");
+        printf("|       3 - Pokemon Center            |\n");
+        printf("|       4 - Quitter le Jeu            |\n");
+        printf("|                                     |\n");
+        printf("+-------------------------------------+\n");
+        printf("Entrez votre choix (1, 2, 3 ou 4) : ");
+        scanf("%d", &Location);
+
+        switch (Location) {
+            case 1:
+                printf("Vous entrez dans la nature.\n");
+                break;
+            case 2:
+                printf("Vous entrez dans le Shop.\n");
+                break;
+            case 3:
+                printf("Vous entrez dans le Pokémon Center.\n");
+                break;
+            case 4:
+                printf("Vous avez quitté le Jeu. À bientôt !\n");
+                return 0; // Quitte le jeu
+            default:
+                printf("Choix invalide. Essayez encore.\n");
+                break;
+        }
+    }
 
     return 0;
 }
